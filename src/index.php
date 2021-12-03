0b7b6d8270d603b5ecc779a51cdc03ef7997c5a7
florianlv <lavieflo100@gmail.com>
2021-12-03 01:32:07 +0100

    message

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-echo '<p>update : 23 nov. 2021 </p>';
+echo '<p>update : 23aaa nov. 2021 </p>';
 phpinfo();
 ?>
 
